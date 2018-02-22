818536a0e30c5f71a83cc90b2b143734db3fb82f
gulrajpw <gulrajpw@clarksonalumni.com>
2018-02-22 12:50:45 -0500

    Error checking on down fhir endpoint
    These FHIR endpoints are unreliable and need checking to see if they are
    up before any search queries are attempted.

--- PatientQueryFHIR/Controllers/HomeController.cs
+++ PatientQueryFHIR/Controllers/HomeController.cs
@@ -16,17 +16,43 @@ namespace PatientQueryFHIR.Controllers
         }
         public ActionResult PatientInfoForm(string patientQuery)
         {
-            
-            PatientInfoModel patientVM = new PatientInfoModel();
-            if (patientQuery != null)
+            bool canSearch = isHostAvailable();
+            if (canSearch)
             {
-                patientVM.FetchQueryResults(patientQuery);
+                PatientInfoModel patientVM = new PatientInfoModel();
 
-            }
+                if (patientQuery != null)
+                {
+                    patientVM.FetchQueryResults(patientQuery);
+
+                }
                 ViewBag.ReturnedResults = patientVM.ReturnPatients();
+            }
+            else
+            {
+                List<string> errorMessage = new List<string>();
+                string endpointDownMsg = "FHIR endpoint is down";
+                errorMessage.Add(endpointDownMsg);
+                ViewBag.ReturnedResults = errorMessage; //Re use the existing returned results field to raise error message.
+            }
 
             return View();
         }
+
+        public bool isHostAvailable() //Before we do anything, let's make sure the endpoint is available. 
+        {
+            try
+            {
+                var ping = new System.Net.NetworkInformation.Ping();
+                var result = ping.Send("http://spark-dstu2.furore.com/fhir");
+                return (result.Status != System.Net.NetworkInformation.IPStatus.Success) ? false : true;
+            }
+            catch (Exception)
+            {
+                return false; //If anything is returned other than Success, endpoint is not available.
+            }
+        }
+
 
     }
 }
--- PatientQueryFHIR/Models/PatientInfoModel.cs
+++ PatientQueryFHIR/Models/PatientInfoModel.cs
@@ -12,26 +12,27 @@ namespace PatientQueryFHIR.Models
     {
         List<Patient> returnedPatients = new List<Patient>();
         int recordsCount;
+        string FHIRendpoint = "http://spark-dstu2.furore.com/fhir"; //http://spark.furore.com/fhir
 
+      
         internal void FetchQueryResults(string userQuery)
         {
             //Hit the endpoint URL and fill into ViewBag when done. 
             
-            if (userQuery != null)
-            {
-             // var endpoint = new Uri("http://spark.furore.com/fhir");
-             var endpoint = new Uri("http://spark-dstu2.furore.com/fhir");
-             var client = new FhirClient(endpoint);
-             var query = new string[] { "name=" + userQuery };
-             var bundle = client.Search("Patient", query);
+                if (userQuery != null)
+                {
+                    var endpoint = new Uri(FHIRendpoint);
+                    var client = new FhirClient(endpoint);
+                    var query = new string[] { "name=" + userQuery };
+                    var bundle = client.Search("Patient", query);
 
-              recordsCount = bundle.Entry.Count();
-              foreach (var entry in bundle.Entry)
-              {
-                Patient p = (Patient)entry.Resource;
-                returnedPatients.Add(p);
-              }
-           }
+                    recordsCount = bundle.Entry.Count();
+                    foreach (var entry in bundle.Entry)
+                    {
+                        Patient p = (Patient)entry.Resource;
+                        returnedPatients.Add(p);
+                    }
+                }   
         }
 
         internal List<String> ReturnPatients()
